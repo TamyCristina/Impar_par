bc7f9444807cb43bf65a8c4f0e288cd5ba60bf41
TamyCristina <112989809+TamyCristina@users.noreply.github.com>
2022-10-24 17:16:21 -0300

    Js impar_par

---
+++ impar_par.js
@@ -0,0 +1,8 @@
+//Exercício para saber se o número X é par ou impar
+
+let numeroX = 12
+if(numeroX % 2 === 0) {
+    console.log("O número X é par");
+}else{
+    console.log("O número X é impar")
+}
